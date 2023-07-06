b4f25f42600716fc8218bf02c70330a7dbb48391
C70Schwager <cyrillgoesweblop@protonmail.com>
2023-07-06 11:10:17 +0200

    new-new-new-new

---
+++ result.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+        <!-- HEADER -->
+    <?php 
+        include "./includes/header.php"; 
+    ?>
+    <!-- END:HEADER -->
+
+
+        <!-- FOOTER -->
+    <?php include "./includes/footer.php"; ?>
+    <!-- END:FOOTER -->
+
+    
+</body>
+</html>
